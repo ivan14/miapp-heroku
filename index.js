08350dbee836de63bd979a99ea891901ffec022f
ivan14 <ivanhuerta14@gmail.com>
2025-10-19 00:21:06 -0600

    Initialize Express app with a greeting endpoint

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo desde Heroku!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
